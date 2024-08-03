df58eaf7f3bf2c0903ceed1ac72925c9ddd1ffee
Oshadi-yashodara <yashodaraoshadi@gmail.com>
2024-08-03 19:32:50 +0530

    modify code

--- src/components/signin/BoxColor.js
+++ src/components/signin/BoxColor.js
@@ -4,10 +4,24 @@ export class BoxColor extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
         <link rel="stylesheet" href="src/components/signin/BoxColor.css">
-            <div class="box">
-                <slot></slot>
-                <h1>Sign In</h1>
+        <div class="box">
+            <h1>Sign In</h1>
+            <form class="signin-form">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+                
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+                
+                <button type="submit" class="signin-button">Sign In</button>
+            </form>
+            <div class="forgot-password">
+                <a href="#">Forgot your password?</a>
             </div>
+            <div class="signup-info">
+                <p>Don't have an account? <a href="#">Sign up here</a></p>
+            </div>
+        </div>
         `;
     }
 }
--- src/pages/return.js
+++ src/pages/return.js
@@ -1,9 +1,27 @@
 const returnItem = () => {
     return (`
-    <link rel="stylesheet" href="src/pages/return.css">
-        <div class="return">
-            <h1>Return</h1>
-        </div>    
+      <link rel="stylesheet" href="src/pages/return.css">
+    <div class="return">
+        <h1>Return</h1>
+        <div class="return-info">
+            <p>If you are not satisfied with your purchase, you can return items within 30 days of receipt. Please follow the instructions below to process your return.</p>
+        </div>
+        <form class="return-form">
+            <label for="order-number">Order Number:</label>
+            <input type="text" id="order-number" name="order-number" required>
+            
+            <label for="item-name">Item Name:</label>
+            <input type="text" id="item-name" name="item-name" required>
+            
+            <label for="return-reason">Reason for Return:</label>
+            <textarea id="return-reason" name="return-reason" rows="4" required></textarea>
+            
+            <button type="submit" class="submit-button">Submit Return Request</button>
+        </form>
+        <div class="contact-info">
+            <p>For further assistance, please <a href="mailto:support@yourstore.com">contact our support team</a>.</p>
+        </div>
+    </div>
     `)
 }
 
