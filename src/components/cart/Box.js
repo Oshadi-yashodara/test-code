58fce293c09675b9f07e58c2b80a6022d2b994fe
Oshadi-yashodara <yashodaraoshadi@gmail.com>
2024-08-03 10:56:46 +0530

    modify BoxFooter

--- src/components/cart/Box.js
+++ src/components/cart/Box.js
@@ -3,11 +3,69 @@ export class Box extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="src/components/cart/Box.css">
-            <div class="box">
-                <slot></slot>
-                <h1>Cart</h1>
+             <link rel="stylesheet" href="src/components/cart/Box.css">
+        <div class="box">
+            <h1>Cart</h1>
+            <div class="cart-items">
+                <!-- Example Cart Item 1 -->
+                <div class="cart-item">
+                    <img src="path-to-item-image.jpg" alt="Item 1 Name">
+                    <div class="item-details">
+                        <p class="item-name">Item 1 Name</p>
+                        <p class="item-description">Description of item 1, size: Medium, color: Blue</p>
+                        <p class="item-price">$49.99</p>
+                        <div class="item-quantity">
+                            <label for="quantity1">Quantity:</label>
+                            <input type="number" id="quantity1" name="quantity1" min="1" value="1">
+                        </div>
+                        <p class="item-subtotal">Subtotal: $49.99</p>
+                    </div>
+                    <button class="remove-item">Remove</button>
+                </div>
+
+                <!-- Example Cart Item 2 -->
+                <div class="cart-item">
+                    <img src="path-to-item-image.jpg" alt="Item 2 Name">
+                    <div class="item-details">
+                        <p class="item-name">Item 2 Name</p>
+                        <p class="item-description">Description of item 2, size: Large, color: Red</p>
+                        <p class="item-price">$79.99</p>
+                        <div class="item-quantity">
+                            <label for="quantity2">Quantity:</label>
+                            <input type="number" id="quantity2" name="quantity2" min="1" value="1">
+                        </div>
+                        <p class="item-subtotal">Subtotal: $79.99</p>
+                    </div>
+                    <button class="remove-item">Remove</button>
+                </div>
             </div>
+
+            <div class="cart-summary">
+                <p>Subtotal: <span class="subtotal">$129.98</span></p>
+                <p>Taxes: <span class="taxes">$10.40</span></p>
+                <p>Shipping: <span class="shipping">$5.00</span></p>
+                <p>Discount: <span class="discount">-$10.00</span></p>
+                <p>Total: <span class="cart-total">$135.38</span></p>
+                <button class="checkout-button">Proceed to Checkout</button>
+                <button class="continue-shopping-button">Continue Shopping</button>
+            </div>
+
+            <div class="additional-info">
+                <p>Estimated Delivery Date: <span class="delivery-date">Aug 10, 2024</span></p>
+                <a href="#" class="save-for-later">Save for Later</a> |
+                <a href="#" class="wishlist">Move to Wishlist</a>
+            </div>
+
+            <div class="security-info">
+                <p>Secure Checkout with:</p>
+                <img src="path-to-secure-payment-icon.png" alt="Secure Payment">
+            </div>
+
+            <div class="customer-support">
+                <p>Need help? <a href="mailto:support@yourclothingstore.com">Contact Customer Support</a></p>
+                <p><a href="#">Return Policy</a></p>
+            </div>
+        </div>
         `;
     }
 }
--- src/components/footer/BoxFooter.js
+++ src/components/footer/BoxFooter.js
@@ -10,58 +10,83 @@ export class BoxFooter extends HTMLElement {
   
     render() {
       this.shadowRoot.innerHTML = `
-      <link rel="stylesheet" href="src/components/footer/BoxFooter.css">
-        <div class="box-footer">
-          <div class="footer-logo">
+        <link rel="stylesheet" href="src/components/footer/BoxFooter.css">
+      <div class="box-footer">
+        <div class="footer-logo">
+          <img src="path-to-your-logo.png" alt="Brand Logo">
+        </div>
+        <div class="footer-column">
+          <h4>About Us</h4>
+          <ul>
+            <li><a href="#">Our Story</a></li>
+            <li><a href="#">Sustainability</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h4>Customer Service</h4>
+          <ul>
+            <li><a href="#">Help & FAQs</a></li>
+            <li><a href="#">Shipping & Delivery</a></li>
+            <li><a href="#">Returns & Exchanges</a></li>
+            <li><a href="#">Track Your Order</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h4>Shop</h4>
+          <ul>
+            <li><a href="#">New Arrivals</a></li>
+            <li><a href="#">Best Sellers</a></li>
+            <li><a href="#">Sale</a></li>
+            <li><a href="#">Gift Cards</a></li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h4>Follow Us</h4>
+          <ul class="social-media">
+            <li><a href="#"><img src="path-to-facebook-icon.png" alt="Facebook"></a></li>
+            <li><a href="#"><img src="path-to-instagram-icon.png" alt="Instagram"></a></li>
+            <li><a href="#"><img src="path-to-twitter-icon.png" alt="Twitter"></a></li>
             
-          </div>
-          <div class="footer-column">
-            <h4>Get to Know Us</h4>
-            <ul>
-              <li><a href="#">Careers</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">About Amazon</a></li>
-              <li><a href="#">Investor Relations</a></li>
-              <li><a href="#">Amazon Devices</a></li>
-              <li><a href="#">Amazon Science</a></li>
-            </ul>
-          </div>
-          <div class="footer-column">
-            <h4>Make Money with Us</h4>
-            <ul>
-              <li><a href="#">Sell products on Amazon</a></li>
-              <li><a href="#">Sell apps on Amazon</a></li>
-              <li><a href="#">Become an Affiliate</a></li>
-              <li><a href="#">Advertise Your Products</a></li>
-              <li><a href="#">Self-Publish with Us</a></li>
-              <li><a href="#">Host an Amazon Hub</a></li>
-            </ul>
-          </div>
-          <div class="footer-column">
-            <h4>Amazon Payment Products</h4>
-            <ul>
-              <li><a href="#">Amazon Rewards Visa Signature Cards</a></li>
-              <li><a href="#">Amazon Store Card</a></li>
-              <li><a href="#">Amazon Secured Card</a></li>
-              <li><a href="#">Amazon Business Card</a></li>
-              <li><a href="#">Amazon Business Line of Credit</a></li>
-              <li><a href="#">Shop with Points</a></li>
-            </ul>
-          </div>
-          <div class="footer-column">
-            <h4>Let Us Help You</h4>
-            <ul>
-              <li><a href="#">Amazon and COVID-19</a></li>
-              <li><a href="#">Your Account</a></li>
-              <li><a href="#">Your Orders</a></li>
-              <li><a href="#">Shipping Rates & Policies</a></li>
-              <li><a href="#">Returns & Replacements</a></li>
-              <li><a href="#">Manage Your Content and Devices</a></li>
-              <li><a href="#">Amazon Assistant</a></li>
-              <li><a href="#">Help</a></li>
-            </ul>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h4>Contact Information</h4>
+          <ul>
+            <li>Phone: (123) 456-7890</li>
+            <li>Email: <a href="mailto:support@yourclothingstore.com">support@yourclothingstore.com</a></li>
+            <li>Address: 123 Fashion St, Style City, Fashionland</li>
+          </ul>
+        </div>
+        <div class="footer-column map-column">
+          <h4>Find Us</h4>
+          <div id="map">
+            <!-- Embed a Google Map here -->
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.469743263348!2d-122.419415384689!3d37.77492977975926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808e51a9c1ab%3A0xb0e672d6a4da2f91!2s123%20Fashion%20St%2C%20Style%20City%2C%20Fashionland!5e0!3m2!1sen!2sus!4v1606745803041!5m2!1sen!2sus"
+              width="100%"
+              height="200"
+              frameborder="0"
+              style="border:0;"
+              allowfullscreen=""
+              aria-hidden="false"
+              tabindex="0"></iframe>
           </div>
         </div>
+        <div class="footer-column search-column">
+          <h4>Search</h4>
+          <form>
+            <input type="text" placeholder="Search our store" />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+        <div class="footer-copyright">
+          <p>&copy; 2024 Your Clothing Store. All rights reserved.</p>
+        </div>
+      </div>
+
       `;
     }
   }
